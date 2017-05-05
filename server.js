257b5a10807adf889af788538f28de07ca44217a
matthewsparr <matthewsparr247@gmail.com>
2017-05-05 15:20:11 +0000

    server 4

--- server.js
+++ server.js
@@ -146,6 +146,6 @@ var express = require('express'),
 app.use(express.static(__dirname + '/client'));
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/client/index.html');
 });
 app.listen(3000);

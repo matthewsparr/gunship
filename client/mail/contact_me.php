26b925b0fd7f828572272c703aec2f40e4ad2c86
matthewsparr <matthewsparr247@gmail.com>
2017-05-07 16:25:17 +0000

    contact me

--- client/mail/contact_me.php
+++ client/mail/contact_me.php
@@ -15,7 +15,6 @@ if(empty($_POST['name'])  		||
    $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = 
 	
 	
 	
@@ -34,33 +33,4 @@ $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
 echo $response->body();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-// Create the email and send the message
-$to = 'gunshipwebdesign@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-$mail=mail($to, "Subject: $email_subject",$message );
-if($mail){
-echo "Thank you for using our mail form";
-}else{
-echo "Mail sending failed."; 
-}
-return true;			
 ?>

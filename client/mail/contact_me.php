bb3162b4a9515705aac48d8edabada49f621ec5e
matthewsparr <matthewsparr247@gmail.com>
2017-05-07 16:34:55 +0000

    contact

--- client/mail/contact_me.php
+++ client/mail/contact_me.php
@@ -8,8 +8,10 @@ if(empty($_POST['name'])  		||
    empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   error_log("got all info");
    {
 	echo "No arguments Provided!";
+	error_log("did not get info");
 	return false;
    }
    $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -23,11 +25,12 @@ $subject = "Contact Form Message";
 $to = new SendGrid\Email(null, "gunshipwebdesign@gmail.com");
 $content = new SendGrid\Content("text/plain", "You have received a new message 
 from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: 
-   $email_address\n\nPhone: $phone\n\nMessage:\n$message");
+$email_address\n\nPhone: $phone\n\nMessage:\n$message");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
+   error_log("about to send email");
 
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();

a25398722c7d9241d79ba4b5288cc34a0db68e26
matthewsparr <matthewsparr247@gmail.com>
2017-05-08 23:59:31 +0000

    revert contact_me

--- client/js/contact_me.js
+++ client/js/contact_me.js
@@ -1,68 +1,42 @@
-/* global $ */
+$(function() {
 
-$("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
-            console.log("got submit error");
+            console.log("got to error");
             // additional error messages or events
         },
- submitSuccess: function($form, event) {
-     
-         console.log("got submit success");
+        submitSuccess: function($form, event) {
+            
+                        console.log("got to success");
 
-     
-         var nodemailer = require("nodemailer");
-
-// create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "gunshipmailer@gmail.com",
-        pass: "gunshipWeb"
-    }
-});
-
-var mailOptions = {
-    from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
-    to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world ✔", // plaintext body
-    html: "<b>Hello world ✔</b>" // html body
-}
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
             
             // get values from FORM
-            mailOptions.from = $("input#email").val();
-            mailOptions.to = "gunshipwebdesign@gmail.com";
-            mailOptions.subject = ("Contact Form Message from " + $("input#name").val());
-            mailOptions.text = ($("textarea#message").val() + "  ---  " + $("input#phone").val());
-            var firstName = $("input#name").val(); 
-            // For Success/Failure Message
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
-                firstName = firstName.split(' ').slice(0, -1).join(' ');
+                firstName = name.split(' ').slice(0, -1).join(' ');
             }
- 
-
-// setup e-mail data with unicode symbols
-
-
-// send mail with defined transport object
-smtpTransport.sendMail(mailOptions, function(error, response){
-    if(error){
-        console.log(error);
-         $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-    }else{
-        console.log("Message sent: " + response.message);
-         $("#btnSubmit").attr("disabled", false);
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
@@ -73,21 +47,31 @@ smtpTransport.sendMail(mailOptions, function(error, response){
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
-    }
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
-    // if you don't want to use this transport object anymore, uncomment following line
-    //smtpTransport.close(); // shut down the connection pool, no more messages
-});
-$("a[data-toggle=\"tab\"]").click(function(e) {
+    $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
         $(this).tab("show");
     });
-    
-    
+});
+
 // When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
     $('#success').html('');
-});
-
-}
 });
